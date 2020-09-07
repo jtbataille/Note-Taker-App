53ad690f527f507cff6d403173bdf1364d40fb6b
Jarrod Bataille <jtbataille@gmail.com>
2020-09-07 15:08:15 -0400

    addinng route files & transfer html routes

--- server.js
+++ server.js
@@ -1,52 +1,18 @@
 // Dependencies
 const express = require("express");
-const path = require("path");
 
 // Sets up the Express App
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var note = [
-    {
-        title: "daddybob",
-        text: "jimmy is a bitch"
-    },
-    {
-        title: "wiejfowie",
-        text: "woeijfowiejfoi"
-    }
-];
+app.use(express.static("public"));
 
-// Route to send the notes page
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-// Route to default to index.html for any unrecoginzed URL additions
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-// Route to read db.json and return saved notes as JSON
-app.get("/api/notes", (req, res) => {
-    // res.sendFile(path.join(__dirname, "/db/db.json"));
-
-    res.json(note);
-});
-
-// Route to receive new, saved note, add to db.json, and return new note
-app.post("/api/notes", (req, res) => {
-
-});
-
-// Route to get ID of note, delete it, and rewrite db.json
-app.delete("/api/notes/:id", (req, res) => {
-
-});
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
 
 // Starts the server to begin listening
 app.listen(PORT, () => {

495332fa4dd4ff4759fdd961cf8c12534b9349e0
Jarrod Bataille <jtbataille@gmail.com>
2020-09-07 16:06:35 -0400

    adding comments to server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 // Sets up the Express App
 const app = express();
+
+// Ensures web server is starting with a dynamic port
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app for data parsing
@@ -11,6 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Accesses the modules found in the route files
 require("./apiRoutes")(app);
 require("./htmlRoutes")(app);
 
